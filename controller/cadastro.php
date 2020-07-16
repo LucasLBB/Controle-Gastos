f9f8170597385dcb3760e874f77019b1fadf0924
Lucas de Lima <lucas2001brito@hotmail.com>
2020-07-16 19:54:35 -0300

    Verificação de Senha

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -5,6 +5,9 @@ $email = $_POST['email'];
 $nome = $_POST['nome'];
 $senha = $_POST['senha'];
 
+//Exige mínimo 10 caracteres, minimo 1 letra minúscula, 1 letra maiúscula, 1 caracter especial e 1 número
+$verificaSenha = preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/", $senha);
+
 //Verifica se existe algo escrito no campo nome e senha
 if(empty($nome) || (empty($senha))) {
     echo '<script type="text/javascript">
@@ -19,6 +22,15 @@ if(filter_var($email, FILTER_VALIDATE_EMAIL) && (!empty($email))) {
 }else {
     echo '<script type="text/javascript">
             alert("Erro ao cadastrar, e-mail Inválido!");
+            window.history.go(-1);
+        </script>';
+    exit;
+}
+
+//Verifica se a senha atende aos requisitos
+if(!$verificaSenha) {
+    echo '<script type="text/javascript">
+            alert("Erro! senha Inválida!");
             window.history.go(-1);
         </script>';
     exit;
